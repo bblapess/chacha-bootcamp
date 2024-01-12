de598a5ee4cbfe6e7c1af61eb942be33ccc54895
Iqbal Pamula Ba'iq <balpamula@gmail.com>
2024-01-13 01:59:52 +0700

    add background hero, benefit section

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,13 @@ window.addEventListener("resize", function () {
 //     }
 //   }
 // });
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
