2de87eea719a511ded1f18a24c03ee5597f5278f
Iqbal Pamula Ba'iq <balpamula@gmail.com>
2024-02-03 01:23:58 +0700

    update: add login page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,56 +6,34 @@ const navbarMenu = document.querySelector(".navbar-menu");
 const navbarButton = document.querySelector(".navbar-button");
 
 menu.onclick = function () {
-    if (navbarMenu.style.display == "flex" && navbarButton.style.display == "flex") {
-        navbarMenu.style.display = "none";
-        navbarButton.style.display = "none";
-    } else {
-        navbarMenu.style.display = "flex";
-        navbarButton.style.display = "flex";
-    }
+	if (
+		navbarMenu.style.display == "flex" &&
+		navbarButton.style.display == "flex"
+	) {
+		navbarMenu.style.display = "none";
+		navbarButton.style.display = "none";
+	} else {
+		navbarMenu.style.display = "flex";
+		navbarButton.style.display = "flex";
+	}
 };
 
 window.addEventListener("resize", function () {
-    if (window.innerWidth > 1024) {
-        navbarMenu.style.display = "flex";
-        navbarButton.style.display = "flex";
-    } else {
-        navbarMenu.style.display = "none";
-        navbarButton.style.display = "none";
-    }
+	if (window.innerWidth > 1024) {
+		navbarMenu.style.display = "flex";
+		navbarButton.style.display = "flex";
+	} else {
+		navbarMenu.style.display = "none";
+		navbarButton.style.display = "none";
+	}
 });
 
-// menu.onclick = function () {
-//     if (navbarButton.style.display == "flex") {
-//         navbarButton.style.display = "none";
-//     } else {
-//         navbarButton.style.display = "flex";
-//     }
-// };
+const header = document.querySelector("header");
 
-// hamburger.addEventListener("click", function () {
-//     navButton.classList.toggle("navbar-button");
-//     navMenu.classList.toggle("navbar-menu");
-// });
-
-// const navbar = document.querySelector('.navbar');
-
-// window.addEventListener('scroll', () => {
-//   if (window.innerWidth > 1024) {
-//     if (window.scrollY > 0) {
-//       navbar.style.padding = '1rem 7rem';
-//     } else {
-//       navbar.style.padding = '4rem 7rem';
-//     }
-//   }
-// });
-
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
+window.addEventListener("scroll", () => {
+	if (window.scrollY > 0) {
+		header.classList.add("scrolled");
+	} else {
+		header.classList.remove("scrolled");
+	}
 });
